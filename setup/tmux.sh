0fcd1a2dfa3d55f8624c91962aad19383e02bf3a
amoreira <anderson.moreira@vtex.com.br>
2017-02-01 10:24:53 -0200

    Install tmp

--- setup/tmux.sh
+++ setup/tmux.sh
@@ -2,3 +2,7 @@ o ">>>"
 echo ">>> Copy tmux.conf"
 echo ">>>"
 cp ./macos/.tmux.conf $HOME/.tmux.conf
+
+echo ">>> Installing tmux plugin manager"
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
